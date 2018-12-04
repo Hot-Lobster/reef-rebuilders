3de203a7d79b905bb395fe436048e7f45cdf508e
Ilya <ilyalutskiy@gmail.com>
2018-12-04 17:10:00 -0600

    layout fix

--- reef-app/src/index.js
+++ reef-app/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
