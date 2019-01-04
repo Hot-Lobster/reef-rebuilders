3ec593d346d7ae1c944bfc711ec4a568797613ab
Joe <dinhx068@umn.edu>
2019-01-04 00:04:22 -0600

    mongo and file hierarchy

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // Exporting an object containing all of our models
 module.exports = {
-    User: require("./User")
-  };
+  Post: require("./Post"),
+  User: require("./User")
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,17 @@
+const path = require("path");
 const router = require("express").Router();
+const postRoutes = require("./posts");
 const userRoutes = require("./user");
+
+// Post route
+router.use("/posts", postRoutes);
 
 // User route
 router.use("/user", userRoutes);
-// Post route
-router.use("/post", userRoutes);
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
 
 module.exports = router;
